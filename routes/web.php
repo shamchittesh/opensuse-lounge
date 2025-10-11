1fdd399ca032c2704ac1924080010820820f550a
Bruno Bernard <contact.brunobernard@gmail.com>
2025-10-11 19:33:33 +0400

    feat: add csv

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\MemberExportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,5 +13,7 @@ Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::resource('members', MemberController::class)
-    ->middleware(['auth']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('members/export', MemberExportController::class)->name('members.export');
+    Route::resource('members', MemberController::class);
+});
