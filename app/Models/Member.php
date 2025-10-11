284dd461ed7c1ab93c32d9a832a294d6c81bb95b
Chittesh Sham <shamchittesh@gmail.com>
2025-10-11 18:43:46 +0400

    feature: create member factory with seed

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    use Hasfactory;
+
+    protected $fillable = [
+        'username',
+        'email_target',
+        'email_nick',
+        'email_full',
+        'libera_nick',
+        'libera_cloak',
+        'libera_cloak_applied',
+        'status',
+    ];
 }
