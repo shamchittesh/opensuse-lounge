6918355fbe34f3722fcdba0585f5230e1eb1f4f5
Bruno Bernard <contact.brunobernard@gmail.com>
2025-10-12 14:17:24 +0400

    feat: redirect to change password when not verified

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\PasswordChangeController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\MemberExportController;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,11 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('members/export', MemberExportController::class)->name('members.export');
-    Route::resource('members', MemberController::class);
+    Route::get('/change-password', [PasswordChangeController::class, 'index'])->name('verification.notice');
+    Route::put('/change-password', [PasswordChangeController::class, 'update'])->name('password.change');
+
+    Route::middleware(['verified'])->group(function () {
+        Route::get('members/export', MemberExportController::class)->name('members.export');
+        Route::resource('members', MemberController::class);
+    });
 });
