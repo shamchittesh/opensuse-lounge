cab88241946c5d4a31668f8c7f16a41f321ef399
Bruno Bernard <contact.brunobernard@gmail.com>
2025-10-12 09:19:12 +0400

    feat: add notification for email

---
+++ app/Notifications/AccountCreated.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AccountCreated extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private string $email, private string $password) {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Your account has been created')
+            ->markdown('mail.new-account-created', [
+                'email' => $this->email,
+                'password' => $this->password,
+            ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ resources/views/mail/new-account-created.blade.php
@@ -0,0 +1,9 @@
+<x-mail::message>
+Your account has been created!
+
+here are your credentials:
+Email: {{ $email }}
+Password: {{ $password }}
+
+Thanks,<br>
+</x-mail::message>
